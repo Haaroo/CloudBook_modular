b1ea1b18645e7258e5fdec62660785935a409137
Emmanuel Haro <emmanuelharo04@gmail.com>
2024-06-06 18:31:07 -0600

    Creacion de seccion libro para apertura de descripcion

--- js/movLibIndex.js
+++ js/movLibIndex.js
@@ -4,107 +4,119 @@ const librosInfantilesContainer = document.querySelector('#libros-infantiles .li
 const librosCienciaContainer = document.querySelector('#libros-ciencia .libros-container-ciencia');
 const librosProgramacionContainer = document.querySelector('#libros-programacion .libros-container-programacion');
 
-async function obtenerLibrosProgramacion() {
-    try {
-        const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=subject:programming&maxResults=5');
-        if (!response.ok) {
-            throw new Error('No se pudo obtener la información de los libros adaptados a películas');
+        async function obtenerLibrosProgramacion() {
+            try {
+                const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=subject:programming&maxResults=5');
+                if (!response.ok) {
+                    throw new Error('No se pudo obtener la información de los libros adaptados a películas');
+                }
+                const data = await response.json();
+                return data.items;
+            } catch (error) {
+                console.error('Error al obtener los libros adaptados a películas:', error);
+                return [];
+            }
         }
-        const data = await response.json();
-        return data.items;
-    } catch (error) {
-        console.error('Error al obtener los libros adaptados a películas:', error);
-        return [];
-    }
-}
 
-async function obtenerLibrosNuevos() {
-    try {
-        const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=new+books&maxResults=20');
-        if (!response.ok) {
-            throw new Error('No se pudo obtener la información de los libros');
+        async function obtenerLibrosNuevos() {
+            try {
+                const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=new+books&maxResults=20');
+                if (!response.ok) {
+                    throw new Error('No se pudo obtener la información de los libros');
+                }
+                const data = await response.json();
+                return data.items;
+            } catch (error) {
+                console.error('Error al obtener los libros:', error);
+                return [];
+            }
         }
-        const data = await response.json();
-        return data.items;
-    } catch (error) {
-        console.error('Error al obtener los libros:', error);
-        return [];
-    }
-}
 
-async function obtenerLibrosTerror() {
-    try {
-        const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=subject:horror&maxResults=5');
-        if (!response.ok) {
-            throw new Error('No se pudo obtener la información de los libros');
+        async function obtenerLibrosTerror() {
+            try {
+                const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=subject:horror&maxResults=5');
+                if (!response.ok) {
+                    throw new Error('No se pudo obtener la información de los libros');
+                }
+                const data = await response.json();
+                return data.items;
+            } catch (error) {
+                console.error('Error al obtener los libros de terror:', error);
+                return [];
+            }
         }
-        const data = await response.json();
-        return data.items;
-    } catch (error) {
-        console.error('Error al obtener los libros de terror:', error);
-        return [];
-    }
-}
 
-async function obtenerLibrosInfantiles() {
-    try {
-        const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=subject:children&orderBy=relevance&maxResults=5');
-        if (!response.ok) {
-            throw new Error('No se pudo obtener la información de los libros');
+        async function obtenerLibrosInfantiles() {
+            try {
+                const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=subject:children&orderBy=relevance&maxResults=5');
+                if (!response.ok) {
+                    throw new Error('No se pudo obtener la información de los libros');
+                }
+                const data = await response.json();
+                return data.items;
+            } catch (error) {
+                console.error('Error al obtener los libros de terror:', error);
+                return [];
+            }
         }
-        const data = await response.json();
-        return data.items;
-    } catch (error) {
-        console.error('Error al obtener los libros de terror:', error);
-        return [];
-    }
-}
 
-async function obtenerLibrosCiencia() {
-    try {
-        const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=subject:science&orderBy=relevance&maxResults=5');
-        if (!response.ok) {
-            throw new Error('No se pudo obtener la información de los libros');
+        async function obtenerLibrosCiencia() {
+            try {
+                const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=subject:science&orderBy=relevance&maxResults=5');
+                if (!response.ok) {
+                    throw new Error('No se pudo obtener la información de los libros');
+                }
+                const data = await response.json();
+                return data.items;
+            } catch (error) {
+                console.error('Error al obtener los libros de terror:', error);
+                return [];
+            }
         }
-        const data = await response.json();
-        return data.items;
-    } catch (error) {
-        console.error('Error al obtener los libros de terror:', error);
-        return [];
-    }
-}
 
-function mostrarLibros(libros, container) {
-    container.innerHTML = '';
-    libros.forEach((libro, index) => {
-        const divLibro = document.createElement('div');
-        divLibro.classList.add('libro');
-        const imagen = document.createElement('img');
-        imagen.src = libro.volumeInfo.imageLinks.thumbnail;
-        imagen.alt = libro.volumeInfo.title;
-        const parrafo = document.createElement('p');
-        parrafo.textContent = `${libro.volumeInfo.title} - ${libro.volumeInfo.authors ? libro.volumeInfo.authors.join(', ') : 'Autor desconocido'}`;
-        divLibro.appendChild(imagen);
-        divLibro.appendChild(parrafo);
-        container.appendChild(divLibro);
-    });
-}
+        function abrirPaginaDetalle(titulo, autor, descripcion) {
+            const url = `detalle_libro.html?titulo=${encodeURIComponent(titulo)}&autor=${encodeURIComponent(autor)}&descripcion=${encodeURIComponent(descripcion)}`;
+            window.open(url, '_blank');
+        }
 
-async function iniciar() {
-    const librosNuevos = await obtenerLibrosNuevos();
-    mostrarLibros(librosNuevos, librosNuevosContainer);
+        function mostrarLibros(libros, container) {
+            container.innerHTML = '';
+            libros.forEach((libro, index) => {
+                const divLibro = document.createElement('div');
+                divLibro.classList.add('libro');
+                const imagen = document.createElement('img');
+                imagen.src = libro.volumeInfo.imageLinks.thumbnail;
+                imagen.alt = libro.volumeInfo.title;
+                imagen.addEventListener('click', function() {
+                    abrirPaginaDetalle(
+                        libro.volumeInfo.title,
+                        libro.volumeInfo.authors ? libro.volumeInfo.authors.join(', ') : 'Autor desconocido',
+                        libro.volumeInfo.description ? libro.volumeInfo.description : 'No hay descripción disponible'
+                    );
+                });
+                const parrafo = document.createElement('p');
+                parrafo.textContent = `${libro.volumeInfo.title} - ${libro.volumeInfo.authors ? libro.volumeInfo.authors.join(', ') : 'Autor desconocido'}`;
+                divLibro.appendChild(imagen);
+                divLibro.appendChild(parrafo);
+                container.appendChild(divLibro);
+            });
+        }
 
-    const librosTerror = await obtenerLibrosTerror();
-    mostrarLibros(librosTerror, librosTerrorContainer);
+        async function iniciar() {
+            const librosNuevos = await obtenerLibrosNuevos();
+            mostrarLibros(librosNuevos, librosNuevosContainer);
 
-    const librosInfantiles = await obtenerLibrosInfantiles();
-    mostrarLibros(librosInfantiles, librosInfantilesContainer);
+            const librosTerror = await obtenerLibrosTerror();
+            mostrarLibros(librosTerror, librosTerrorContainer);
 
-    const librosCiencia = await obtenerLibrosCiencia();
-    mostrarLibros(librosCiencia, librosCienciaContainer);
+            const librosInfantiles = await obtenerLibrosInfantiles();
+            mostrarLibros(librosInfantiles, librosInfantilesContainer);
 
-    const librosProgramacion = await obtenerLibrosProgramacion();
-    mostrarLibros(librosProgramacion, librosProgramacionContainer);
-}
+            const librosCiencia = await obtenerLibrosCiencia();
+            mostrarLibros(librosCiencia, librosCienciaContainer);
 
-iniciar();
+            const librosProgramacion = await obtenerLibrosProgramacion();
+            mostrarLibros(librosProgramacion, librosProgramacionContainer);
+        }
+
+        iniciar();
